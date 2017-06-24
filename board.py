7a2bb7c9ac70fe44cacaa19aea33100d2cd36264
Aron Granberg <aron.granberg@gmail.com>
2017-06-24 20:08:24 +0200

    Improve code structure

--- board.py
+++ board.py
@@ -15,10 +15,11 @@ def rect_area(rect):
 
 
 class Card:
-    def __init__(self, word, pos, points):
+    def __init__(self, word, rect):
         self.word = word
-        self.pos = pos
-        self.points = points
+        self.rect = rect
+        self.pos = rect[0]
+        self.rect_vertices = [[int(x), int(y)] for x, y in cv2.boxPoints(self.rect)]
 
     def __repr__(self):
         return self.word
@@ -52,11 +53,10 @@ def fit_grid_to_words(cards):
         output[j//5][j%5] = cards[i].word
     return output
 
+
 def find_contours(im, dilation, min_contour_area, max_contour_area):
-    # show(blur1)
     edges = cv2.Canny(im, 40, 100)
     # show(edges)
-    # blur = cv2.GaussianBlur(edges, (41, 41), 0)
 
     element = cv2.getStructuringElement(cv2.MORPH_RECT,
                                         (2 * dilation + 1, 2 * dilation + 1),
@@ -64,8 +64,6 @@ def find_contours(im, dilation, min_contour_area, max_contour_area):
     dilated = cv2.dilate(edges, element)
     # show(dilated)
 
-    # retval, labels = cv2.connectedComponents(dilated)
-    # print(retval, labels)
     contIm, contours, hierarchy = cv2.findContours(dilated, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
     # im2 = im.copy()
     # cv2.drawContours(im2, contours, -1, (255, 255, 255), 2)
@@ -87,40 +85,40 @@ def find_contours(im, dilation, min_contour_area, max_contour_area):
 
 
 def find_text_in_contours(contours, image, word_list):
+    def align_rect_horizontal(rect):
+        if rect[2] < -45:
+            rect = (rect[0], (rect[1][1], rect[1][0]), rect[2] + 90)
+        if rect[2] > 45:
+            rect = (rect[0], (rect[1][1], rect[1][0]), rect[2] - 90)
+        return rect
+
+    def extract_rect_with_perspective(rect):
+        rectVertices = cv2.boxPoints(rect)
+        width = int(rect[1][0])
+        height = int(rect[1][1])
+        h = np.array([[0, height], [0, 0], [width, 0], [width, height]], np.float32)
+        transform = cv2.getPerspectiveTransform(rectVertices, h)
+        warp = cv2.warpPerspective(image, transform, (width, height))
+        return warp
+
     noSpaces2words = dict((w.upper().replace(" ", ""), w) for w in word_list)
     with tesserocr.PyTessBaseAPI() as tess:
         for c in contours:
-            rect = cv2.minAreaRect(c)
-            if rect[2] < -45:
-                rect = (rect[0], (rect[1][1], rect[1][0]), rect[2] + 90)
-            if rect[2] > 45:
-                rect = (rect[0], (rect[1][1], rect[1][0]), rect[2] - 90)
+            rect = align_rect_horizontal(cv2.minAreaRect(c))
 
             # Skip rectangles that are rotated too much (not likely to be words)
             if abs(rect[2]) > 20:
                 continue
 
-            r = cv2.boxPoints(rect)
-
-            width = int(rect[1][0])
-            height = int(rect[1][1])
-            h = np.array([[0, height], [0, 0], [width, 0], [width, height]], np.float32)
-            transform = cv2.getPerspectiveTransform(r, h)
-            warp = cv2.warpPerspective(image, transform, (width, height))
-
-            # warp = ((warp - np.min(warp)).astype(np.float) * (255.0 / (np.max(warp) - np.min(warp)))).astype(np.uint8)
-
-            i = Image.fromarray(warp)
-            tess.SetImage(i)
+            patch = extract_rect_with_perspective(rect)
+            tess.SetImage(Image.fromarray(patch))
 
             result = tess.GetUTF8Text().replace(" ", "").strip().upper()
 
             if result in noSpaces2words:
                 result = noSpaces2words[result]
 
-            points = [[int(x), int(y)] for x, y in r]
-            pos = (int(sum(x for x,y in r) / len(r)), int(sum(y for x,y in r) / len(r)))
-            yield Card(result, pos, points)
+            yield Card(result, rect)
 
 
 def unique(words):
@@ -174,7 +172,7 @@ def find_words(imagePath):
     # cimg = im.copy()
     # cv2.drawContours(cimg, contours, -1, (0, 255, 0), 2)
     # for w in foundWords:
-    #     rects = np.array([w.points])
+    #     rects = np.array([w.rect_vertices])
     #     color = (255, 255, 255) if w in actualWords else (255, 0, 0)
     #     cv2.polylines(cimg, rects, True, color, 2)
     # show(cimg)
