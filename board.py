73b906981c2f7cce8b57f675101e7d0777f92aca
Aron Granberg <aron.granberg@gmail.com>
2017-06-24 23:36:48 +0200

    Tweak optimization and remove debug output

--- board.py
+++ board.py
@@ -54,9 +54,9 @@ def calculate_optimized_bounding_rect(words, im):
             d = -cv2.pointPolygonTest(x, p, True)
             d += padding
             d = max(d, 0)
-            cost += d + d*d*2.5
+            cost += (d + d*d*2.5) * 0.0001
 
-        cost += poly_area(x) * 0.01
+        cost += poly_area(x) * 0.000001
         return cost
 
     x0 = np.array([
@@ -66,7 +66,7 @@ def calculate_optimized_bounding_rect(words, im):
         [0, 0],
     ]).reshape([8])
 
-    xf = scipy.optimize.fmin_cg(loss, x0, epsilon=0.001)
+    xf = scipy.optimize.fmin_cg(loss, x0, epsilon=0.1, gtol=0.0001, disp=False)
     ret = xf.reshape([4, 2]).tolist()
     ret[0][0] += padding
     ret[0][1] -= padding
