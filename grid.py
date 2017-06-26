ba30978153cb2a7df0445f5e0fc0b59c98f59cf5
Simon Lindholm <simon.lindholm10@gmail.com>
2017-06-26 16:00:31 +0200

    More work on grid

--- grid.py
+++ grid.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 import cv2
 import sys
+import math
 from termcolor import colored
 
 
 SIZE = 5
+INF = 10**9
 
 
 def show(im):
@@ -63,99 +65,135 @@ def dfs_segmentation(im, minimumArea, maximumArea):
             if bbarea < minimumArea or bbarea > maximumArea:
                 continue
             box = (minX, minY, maxX, maxY)
-            # print("found box " + str(box) + " " + str(bbarea))
-            # box = clamp_aabb(expand_aabb(box, 3), width, height)
-            # averageR = totR/totPixels
-            # averageG = totG/totPixels
-            # averageB = totB/totPixels
             yield box
-            # yield (averageR, averageG, averageB, box)
+
+
+def fit_grid2(points, topleft, topright, botleft, botright):
+    def gridp(i, j):
+        a = topleft + j * (topright - topleft)
+        b = botleft + j * (botright - botleft)
+        return b * i + a * (1 - i)
+
+    def top(c):
+        return (int(c.imag), int(c.real))
+
+    match_dis2 = 20**2
+    minr, maxr = -SIZE+2, SIZE
+    scores = [[0] * (maxr - minr) for _ in range(maxr - minr)]
+    for i in range(minr, maxr):
+        for j in range(minr, maxr):
+            p = gridp(i, j)
+            best_dis2 = INF
+            for k in range(len(points)):
+                q = complex(points[k][0], points[k][1])
+                dis = p - q
+                best_dis2 = min(best_dis2, dis.real**2 + dis.imag**2)
+            score = max(1 - best_dis2 / match_dis2, 0)
+            # rat = best_dis2 / match_dis2
+            # score = 1 - rat if rat < 0.5 else math.exp(-rat) * 0.82436
+            # score = 1 if best_dis2 < match_dis2 else 0
+            scores[i - minr][j - minr] = score
+
+    bestsc = (-1, 0, 0)
+    for i in range(minr, 1):
+        for j in range(minr, 1):
+            sc = 0
+            for ik in range(SIZE):
+                for jk in range(SIZE):
+                    sc += scores[i + ik - minr][j + jk - minr]
+            bestsc = max(bestsc, (sc, i, j))
+
+    score = bestsc[0]
+    assert score != -1
+    bi = bestsc[1]
+    bj = bestsc[2]
+    grid = [[top(gridp(bi + i, bj + j)) for j in range(SIZE)] for i in range(SIZE)]
+    return (score, grid)
 
 
 def fit_grid(points):
-    inf = 10**9
     bestscore = -1
     bestgrid = None
+    bestcorners = None
+    min_dist = 20
     for pt in points:
         # Find a reasonable subsquare (pt, bestr, bestd, bestrd)
-        bestrx = bestdy = inf
+        bestrx = bestdy = INF
         bestr = bestd = None
         for pt2 in points:
             dy = pt2[0] - pt[0]
             dx = pt2[1] - pt[1]
-            if 0 < dx < bestrx and abs(dy) < dx * 0.6:
+            if min_dist < dx < bestrx and abs(dy) < dx * 0.6:
                 bestrx = dx
                 bestr = pt2
-            if 0 < dy < bestdy and abs(dx) < dy * 0.6:
+            if min_dist < dy < bestdy and abs(dx) < dy * 0.6:
                 bestdy = dy
                 bestd = pt2
         if not bestr or not bestd:
             continue
-        bestrx = bestdy = inf
+        bestrx = bestdy = INF
         bestdr = bestrd = None
         for pt2 in points:
             dy = pt2[0] - bestd[0]
             dx = pt2[1] - bestd[1]
-            if 0 < dx < bestrx and abs(dy) < dx * 0.6:
+            if min_dist < dx < bestrx and abs(dy) < dx * 0.6:
                 bestrx = dx
                 bestdr = pt2
             dy = pt2[0] - bestr[0]
             dx = pt2[1] - bestr[1]
-            if 0 < dy < bestdy and abs(dx) < dy * 0.6:
+            if min_dist < dy < bestdy and abs(dx) < dy * 0.6:
                 bestdy = dy
                 bestrd = pt2
         if bestdr != bestrd or not bestrd:
             continue
 
-        # Try basing our grid on those four points, and see what fits.
+        # Try basing our grid on those four points, and see what fits
         topleft = complex(pt[0], pt[1])
         topright = complex(bestr[0], bestr[1])
         botleft = complex(bestd[0], bestd[1])
         botright = complex(bestrd[0], bestrd[1])
-        def gridp(i, j):
-            a = topleft + j * (topright - topleft)
-            b = botleft + j * (botright - botleft)
-            return b * i + a * (1 - i)
-        # assert gridp(0,0) == topleft
-        # assert gridp(1,0) == botleft
-        # assert gridp(0,1) == topright
-        # assert gridp(1,1) == botright
-
-        def top(c):
-            return (int(c.imag), int(c.real))
-
-        match_dis2 = 10**2
-        minr, maxr = -SIZE+2, SIZE
-        scores = [[0] * (maxr - minr) for _ in range(maxr - minr)]
-        for i in range(minr, maxr):
-            for j in range(minr, maxr):
-                p = gridp(i, j)
-                best_dis2 = inf
-                for k in range(len(points)):
-                    q = complex(points[k][0], points[k][1])
-                    dis = p - q
-                    best_dis2 = min(best_dis2, dis.real**2 + dis.imag**2)
-                scores[i - minr][j - minr] = 1 if best_dis2 < match_dis2 else 0
-
-        bestsc = (-1, 0, 0)
-        for i in range(minr, 1):
-            for j in range(minr, 1):
-                sc = 0
-                for ik in range(SIZE):
-                    for jk in range(SIZE):
-                        sc += scores[i + ik - minr][j + jk - minr]
-                bestsc = max(bestsc, (sc, i, j))
-
-        score = bestsc[0]
-        assert score != -1
-        bi = bestsc[1]
-        bj = bestsc[2]
+        score, grid = fit_grid2(points, topleft, topright, botleft, botright)
         if score > bestscore:
             bestscore = score
-            bestgrid = [[top(gridp(bi + i, bj + j)) for j in range(SIZE)] for i in range(SIZE)]
+            bestgrid = grid
+            bestcorners = (topleft, topright, botleft, botright)
 
-    if bestscore <= SIZE*SIZE//2:
+    if not bestgrid: # or bestscore <= SIZE*SIZE//2:
         return None
+
+    # Hill climb a bit to improve the solution
+    for delta in [2., 1., .5, .25]:
+        DIRY = [complex(delta * i, 0) for i in range(-1, 2)]
+        DIRX = [complex(0, delta * i) for i in range(-1, 2)]
+        for dira in DIRY:
+            for dirb in DIRY:
+                for dirc in DIRY:
+                    for dird in DIRY:
+                        c = bestcorners
+                        ca, cb, cc, cd = c[0] + dira, c[1] + dirb, c[2] + dirc, c[3] + dird
+                        score, grid = fit_grid2(points, ca, cb, cc, cd)
+                        if score > bestscore:
+                            bestscore = score
+                            bestgrid = grid
+                            bestcorners = (ca, cb, cc, cd)
+        for dira in DIRX:
+            for dirb in DIRX:
+                for dirc in DIRX:
+                    for dird in DIRX:
+                        c = bestcorners
+                        ca, cb, cc, cd = c[0] + dira, c[1] + dirb, c[2] + dirc, c[3] + dird
+                        score, grid = fit_grid2(points, ca, cb, cc, cd)
+                        if score > bestscore:
+                            bestscore = score
+                            bestgrid = grid
+                            bestcorners = (ca, cb, cc, cd)
+
+
+    # c = bestcorners
+    # ca, cb, cc, cd = c[0], c[1], c[2] + complex(-1.0, 0.), c[3] + complex(-2.7, -0.0)
+    # bestscore, bestgrid = fit_grid2(points, ca, cb, cc, cd)
+
+    print(bestscore)
     return bestgrid
 
 
@@ -194,12 +232,10 @@ def find_grid(fname):
     # show(contIm)
 
     areas = list(dfs_segmentation(gray, 80, 512**2 // 20))
-    points = [((ar[1] + ar[3]) // 2, (ar[0] + ar[2]) // 2) for ar in areas]
+    points = [((ar[1] + ar[3]) / 2, (ar[0] + ar[2]) / 2) for ar in areas]
     print(len(points))
 
     grid = fit_grid(points)
-
-    print(grid)
 
     mat = []
     for row in grid:
@@ -219,7 +255,7 @@ def find_grid(fname):
                 for j in range(11):
                     gray[max(min(y+i, height-1), 0)][max(min(x+j, width-1), 0)] = 128
 
-    # show(gray)
+    show(gray)
     return mat
 
 
