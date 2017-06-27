94a123e72d75f38a1a74e79adbf87e35c8cb0751
Aron Granberg <aron.granberg@gmail.com>
2017-06-28 00:11:27 +0200

    Improve montage script

--- montage.py
+++ montage.py
@@ -7,15 +7,16 @@ if len(sys.argv) < 3:
     print("\nExample:\n\tpython3 montage.py grid.py examples/secret/*")
     exit(1)
 
-call("rm res*.png", shell=True)
+call("rm -rf res", shell=True)
+os.mkdir("res")
 samples = []  # type: List[float]
 cnt = 0
 for file in sys.argv[2:]:
     call(["python3", sys.argv[1], file])
-    os.rename("res.png", "res" + str(cnt) + ".png")
+    os.rename("res.png", "res/" + os.path.basename(os.path.splitext(file)[0]) + ".png")
     cnt += 1
 
-call("gm montage -geometry 300x -tile 5x res*.png out.png", shell=True)
+call("gm montage -geometry 300x -tile 5x -label \"%f\" res/*.png out.png", shell=True)
 if sys.platform.startswith('linux'):
     call(["xdg-open", "out.png"])
 elif sys.platform == 'darwin':
